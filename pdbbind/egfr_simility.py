2c4fad5faa79928083580fbf51645f2fa2d1f09f
xh <983187209@qq.com>
2019-07-22 14:16:13 +0800

    modify

--- pdbbind/egfr_simility.py
+++ pdbbind/egfr_simility.py
@@ -15,15 +15,15 @@ def MACCS_keys_similarity(mol_1, mol_2):
 
 def ligand_file(path):
         if os.path.isfile(path):
-                shutil.copy(path, '/home/xh/ligand_dir')
+                shutil.copy(path, './ligand_dir')
 
 
 def mol2_mol(mol2_file):
     mol_1 = Chem.MolFromMol2File(mol2_file)
-    mol_2 = Chem.MolFromMol2File('/home/xh/gefitinib.mol2')
+    mol_2 = Chem.MolFromMol2File('./gefitinib.mol2')
     return MACCS_keys_similarity(mol_1, mol_2)
 
-file_path = '/home/xh/Documents/databases/egfr_pdbid.txt'
+file_path = './egfr_pdbid.txt'
 with open(file_path, 'r') as f:
         pdb_ligand = []
         pdb_id = []
@@ -35,11 +35,11 @@ with open(file_path, 'r') as f:
 list1 = []
 list2 = []
 for dre in pdb_id:
-        new_dre = '/home/xh/Documents/databases/pdbbind_v2015/' + str(dre.strip()) + '//' + str(dre.strip() + '_ligand.mol2')
+        new_dre = './databases/pdbbind_v2015/' + str(dre.strip()) + '//' + str(dre.strip() + '_ligand.mol2')
         #print new_dre
         ligand_file(new_dre)
         list1.append(str(mol2_mol(new_dre)))
-with open('/home/xh/ligand_dir/gefitinib.txt', 'w') as f1:
+with open('./ligand_dir/gefitinib.txt', 'w') as f1:
     for i, m in zip(pdb_ligand, list1):
         f1.write(str(i + '\t' + m + '\n'))
 
--- pdbbind/spider_drugbank.py
+++ pdbbind/spider_drugbank.py
@@ -2,7 +2,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import requests
-file_path = '/home/dongha/Desktop/PP_score_topk3com202node2vec100.csv'
+file_path = './PP_score_topk3com202node2vec100.csv'
 with open(file_path, 'r') as f:
     text = f.readlines()
     for i in text[61:73]:
@@ -10,6 +10,6 @@ with open(file_path, 'r') as f:
         id = i_[3].split()[0]
         read_path = 'https://www.drugbank.ca/structures/small_molecule_drugs/' + id + '.sdf?type=3d'
         t = requests.get(read_path)
-        write_path = '/home/dongha/Desktop/drugs/' + id + '.sdf'
+        write_path = './drugs/' + id + '.sdf'
         with open(write_path, 'w') as f1:
             f1.write(t.text)
